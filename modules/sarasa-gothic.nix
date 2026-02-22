64febf932113c7b8dab2f843beff6fe76f8e8a0e
Lemuel DLS <noreply@git.lemueldls.dev>
2026-02-22 02:45:53 -0500

    chore: update sarasa gothic font

--- modules/sarasa-gothic.nix
+++ modules/sarasa-gothic.nix
@@ -2,6 +2,7 @@
   inputs,
   lib,
   stdenv,
+  fetchurl,
 
   # Deps
   callPackage,
@@ -17,8 +18,9 @@ sarasa-gothic.overrideAttrs (finalAttrs: {
   installPhase = ''
     runHook preInstall
 
-    ${lib.getExe rubify} *.ttc --font ${iosevka.slim}/share/fonts/truetype/IosevkaSlim-Regular.ttf --subset -o dist
-    install -Dm644 -t $out/share/fonts/truetype dist/*.ttc
+    ${lib.getExe rubify} *.ttc -o phase1 --ruby pinyin --position top --font ${iosevka.slim}/share/fonts/truetype/IosevkaSlim-Regular.ttf
+    ${lib.getExe rubify} phase1/*.ttc -o phase2 --ruby romaji --position bottom --font ${iosevka.slim}/share/fonts/truetype/IosevkaSlim-Regular.ttf
+    install -Dm644 -t $out/share/fonts/truetype phase2/*.ttc
 
     runHook postInstall
   '';
